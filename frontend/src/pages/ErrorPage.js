235230024f739bda2bfc488d9ef7cbc1ab01ea88
naeem4633 <ahmedn3700@gmail.com>
2023-08-16 18:16:21 +0500

    Added 404 to error page

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -9,12 +9,12 @@ const ErrorPage = () => {
         <div className="flex flex-col items-center w-full h-[50vh]">
             <div className='w-full h-full flex flex-col justify-evenly'>
                 <div className='flex flex-col heading items-center'>
-                    <p className='text-white text-6xl font-bold'>404</p>
-                    <p className='text-xl lg:text-4xl font-semibold text-red-600'>Page Not Found</p> 
+                    <p className='text-6xl font-bold'>404</p>
+                    <p className='text-xl lg:text-4xl font-semibold text-black'>Page Not Found</p> 
                 </div>
             </div>
             <Link to={'/'} className='w-full lg:w-3/4 items-center p-8'>
-                <div className='mx-auto flex flex-row items-center justify-evenly w-52 h-12 rounded text-white color-secondary hover:bg-[#96205d] tracking-wider'>
+                <div className='mx-auto flex flex-row items-center justify-evenly w-52 h-12 rounded text-white bg-gray-900 tracking-wider'>
                     <button className=''>Back to Home Page</button>
                 </div>
             </Link>
